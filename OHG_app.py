51ea5f7393c759a2edb72a78f0720fbc221c20b6
glazunova <glazunovaiv@yandex.ru>
2017-12-07 22:27:28 +0300

    flask-app, llink to css

---
+++ OHG_app.py
@@ -0,0 +1,26 @@
+import os.path
+from urllib.parse import quote, unquote
+from flask import Flask
+from flask import render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/search')
+def search():
+	return render_template('search.html')
+
+@app.route('/texts')
+def texts():
+	return render_template('texts.html')
+
+@app.route('/stats')
+def ststs():
+	return render_template('stats.html')
+
+if __name__ == '__main__':
+	app.run()
