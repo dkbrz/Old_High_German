ff0c92ade977352af6511c9a6f7b933aa070b77a
glazunova <glazunovaiv@yandex.ru>
2018-02-01 18:40:28 +0300

    blocks - main navbar

--- OHG_app.py
+++ OHG_app.py
@@ -1,27 +1,43 @@
 import os.path
 from urllib.parse import quote, unquote
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask import render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+
 @app.route('/search')
 def search():
 	return render_template('search.html')
+
 
 @app.route('/texts')
 def texts():
 	return render_template('texts.html')
 
+
+@app.route('/dictionary')
+def dictionary():
+	return render_template('dictionary.html')
+
+
 @app.route('/stats')
 def stats():
 	return render_template('stats.html')
 
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
 if __name__ == '__main__':
-        app.run()
+    app.run()
 	#app.run(debug=True)
